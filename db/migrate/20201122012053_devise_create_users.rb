d3c5e16f517fd9f6dc004369314e7a4359cdfef5
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-22 13:42:37 +0900

    トップ画面から新規登録画面へのリンク設定

--- db/migrate/20201122012053_devise_create_users.rb
+++ db/migrate/20201122012053_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname ,          null: false
       t.string :last_name ,         null: false
-      t.string :first_name ,        null: false
+      t.string :First_name ,        null: false
       t.string :last_kana ,         null: false
       t.string :first_kana ,        null: false
       t.date :birthday,             null: false
