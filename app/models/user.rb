930badf4bf0201acc8a6b3668ac7276e211debe7
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-22 22:43:09 +0900

    パスワードに英数字を必要とする条件の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_kana, presence: true
