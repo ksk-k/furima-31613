64396b8badc88d21f7c444975615a30257f2ccf6
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-24 18:42:43 +0900

    コメントに対する修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+    VALID_PASSWORD_REGEX = /\A(?:\p{hiragana}|\p{katakana}|[ー－]|[一-龠々])+\z/
     validates :last_name, format: { with: VALID_PASSWORD_REGEX }
-    VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
     validates :first_name, format: { with: VALID_PASSWORD_REGEX }
     VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
     validates :last_kana, format: { with: VALID_PASSWORD_REGEX }
-    VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
     validates :first_kana, format: { with: VALID_PASSWORD_REGEX }
     validates :birthday
   end
