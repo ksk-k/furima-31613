2d0d8b97cd359f8cc35a0e527732a5cc2c6d0ab7
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-24 12:13:10 +0900

    with_optionsを用いてpresence: trueをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+    validates :last_name, format: { with: VALID_PASSWORD_REGEX }
+    VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+    validates :first_name, format: { with: VALID_PASSWORD_REGEX }
+    VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
+    validates :last_kana, format: { with: VALID_PASSWORD_REGEX }
+    VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
+    validates :first_kana, format: { with: VALID_PASSWORD_REGEX }
+    validates :birthday
+  end
 
-  validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
-  validates :last_name, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
-  validates :first_name, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
-  validates :last_kana, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
-  validates :first_kana, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  validates :birthday, presence: true
 end
 
 
