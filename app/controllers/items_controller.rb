ce6df25036e570edcae9f8b5780a5dcd5ec783bd
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-25 15:47:07 +0900

    new,createアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+
+  end
+
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
-  
+
 end
