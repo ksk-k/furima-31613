5de324b981fa16e4dc3b9bdaf59b4551a73c88f1
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-25 15:42:54 +0900

    item.rbにバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validataes :name
+    validataes :detail
+    validataes :category_id
+    validataes :condition_id
+    validataes :delivery_fee_id
+    validataes :prefecture_id
+    validataes :shipping_date_id
+    validataes :price
+  end
 end
