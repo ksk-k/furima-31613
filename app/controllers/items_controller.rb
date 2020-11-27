3b454abdb92cf5ab7e5c5b2e691b99bf1a702eee
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-27 17:55:32 +0900

    コメントに対する修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! 
+
   def index
     @items = Item.includes(:user)
   end
 
   def new
     @item = Item.new
-    
   end
 
   def create
