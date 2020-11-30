fbdb636da54919cf0e3f8b0ec1e07e168ef20daa
ksk-k <kitaichikosuke0203@gmail.com>
2020-11-30 17:07:17 +0900

    商品の表示、出品者の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index]
 
   def index
-    # @items = Item.includes(:user)
+    @items = Item.all
   end
 
   def new
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
