232669e16adbfed6ca6f5c6664c1f1340cac71a9
ksk-k <kitaichikosuke0203@gmail.com>
2020-12-02 13:10:42 +0900

    コメントに対する修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-     @item = Item.find(params[:id])
+     
   end
 
   def edit
-     @item = Item.find(params[:id])
      redirect_to root_path unless current_user.id == @item.user_id
   end
 =begin 
