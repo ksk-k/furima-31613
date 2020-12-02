47da2296f7b1fc3a02962804d71b069c30e6c59b
ksk-k <kitaichikosuke0203@gmail.com>
2020-12-02 14:12:03 +0900

    コメントに対する修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
-                :set_item,only:[:edit, :show]
+  before_action :set_item,only:[:edit, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
